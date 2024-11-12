2bbdac3b25f3c46837a94454fb27c97ab0a8c2e0
YourSmallFriend <SamKint10@hotmail.com>
2024-11-12 16:43:34 +0100

    Eto Forms toegevoegd

---
+++ Huisart-Eto/Huisart-Eto.Gtk/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+
+namespace Huisart_Eto.Gtk
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Eto.Platforms.Gtk).Run(new MainForm());
+        }
+    }
+}
