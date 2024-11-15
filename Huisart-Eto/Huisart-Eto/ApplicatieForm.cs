2198d8ac3be0a6b777c6c007cd4817a63579e217
Samuel <SamKint10@hotmail.com>
2024-11-15 23:19:03 +0100

    het ziet er mooier uit

--- Huisart-Eto/Huisart-Eto/ApplicatieForm.cs
+++ Huisart-Eto/Huisart-Eto/ApplicatieForm.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Eto.Drawing;
 using Eto.Forms;
@@ -6,28 +8,114 @@ namespace Huisart_Eto
 {
     public class ApplicatieForm : Form
     {
+        private const int ItemsPerPage = 25;
+        private int currentPage = 0;
+        private List<Patienten> allPatienten;
+        private GridView gridView;
+
         public ApplicatieForm()
         {
             Title = "Applicatie";
             Maximize();
-            BackgroundColor = Color.FromArgb(50 , 50 , 50);
+            BackgroundColor = Color.FromArgb(50, 50, 50);
 
-            // Retrieve the list of patients from the service
+            // Haal patiënten op
             var patientenService = new PatientenService();
-            var patienten = patientenService.GetPatienten();
+            allPatienten = patientenService.GetPatienten().ToList();
 
-            // Create a GridView and bind the list of patients
-            var gridView = new GridView
+            // Maak een GridView
+            gridView = new GridView();
+            UpdateGridView();
+
+            // Zoekbalk
+            var search = new TextBox();
+            search.TextChanged += (sender, e) =>
             {
-                DataStore = patienten
+                var searchText = search.Text.ToLower();
+                var searchResult = allPatienten
+                    .Where(p => p.first_name.ToLower().Contains(searchText) || 
+                                p.last_name.ToLower().Contains(searchText) ||
+                                p.email.Contains(searchText) || 
+                                p.phone.Contains(searchText) || 
+                                p.postcode.Contains(searchText) || 
+                                p.adress.Contains(searchText) || 
+                                p.place.Contains(searchText))
+                    .ToList();
+                allPatienten = searchResult;
+                currentPage = 0;
+                UpdateGridView();
+            };
+            
+            //als de zoekbalk leeg is dan worden alle patienten getoond
+            search.TextChanged += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(search.Text))
+                {
+                    allPatienten = patientenService.GetPatienten().ToList();
+                    currentPage = 0;
+                    UpdateGridView();
+                }
             };
 
-            // Define columns for the GridView
+            // Navigatieknoppen
+            var previousButton = new Button { Text = "Vorige" };
+            previousButton.Click += (sender, e) =>
+            {
+                if (currentPage > 0)
+                {
+                    currentPage--;
+                    UpdateGridView();
+                }
+            };
+
+            var nextButton = new Button { Text = "Volgende" };
+            nextButton.Click += (sender, e) =>
+            {
+                if ((currentPage + 1) * ItemsPerPage < allPatienten.Count)
+                {
+                    currentPage++;
+                    UpdateGridView();
+                }
+            };
+
+            // Layout
+            Content = new StackLayout
+            {
+                Spacing = 5,
+                Items =
+                {
+                    search,
+                    gridView,
+                    new StackLayout
+                    {
+                        HorizontalContentAlignment = HorizontalAlignment.Center,
+                        Orientation = Orientation.Horizontal,
+                        Spacing = 10,
+                        Items =
+                        {
+                            previousButton,
+                            nextButton
+                        }
+                    }
+                }
+            };
+        }
+
+        private void UpdateGridView()
+        {
+            var pageData = allPatienten
+                .Skip(currentPage * ItemsPerPage)
+                .Take(ItemsPerPage)
+                .ToList();
+
+            gridView.DataStore = pageData;
+
+            // Columns opnieuw definiëren
+            gridView.Columns.Clear();
             gridView.Columns.Add(new GridColumn
             {
                 HeaderText = "ID",
                 DataCell = new TextBoxCell { Binding = Binding.Property<Patienten, int>(p => p.patient_id).Convert(r => r.ToString()) }
-                
             });
             gridView.Columns.Add(new GridColumn
             {
@@ -64,24 +152,6 @@ namespace Huisart_Eto
                 HeaderText = "Place",
                 DataCell = new TextBoxCell { Binding = Binding.Property<Patienten, string>(p => p.place) }
             });
-
-            //maak een zoek balk voor de patienten haal de patienten met een linq query op en zet ze in de gridview
-            var search = new TextBox();
-            search.TextChanged += (sender, e) =>
-            {
-                var searchResult = patienten.Where(p => p.first_name.ToLower().Contains(search.Text) || p.last_name.ToLower().Contains(search.Text) || p.email.Contains(search.Text) || p.phone.Contains(search.Text) || p.postcode.Contains(search.Text) || p.adress.Contains(search.Text) || p.place.Contains(search.Text)).ToList();
-                gridView.DataStore = searchResult;
-            };
-            
-            Content = new StackLayout
-            {
-                Items =
-                {
-                    search,
-                    gridView
-                }
-            };
-            
         }
     }
 }
